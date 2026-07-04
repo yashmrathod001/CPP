1f7a523617bbf0cd2d7971025370f581748f8187
yashmrathod001 <yashmrathod123@gmail.com>
2026-07-04 17:15:30 +0530

    first patturn

---
+++ auto functions 50Q/star patturns/try1.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cout << "enter a int :" << endl;
+    cin >> n;
+
+    for(int i=0; i < n ; i++)
+    {
+        for(int j =0 ; j <n ; j++)
+        {
+            cout<< (j+1);
+        }
+        cout<<"\n";
+    }
+
+    return 0;
+}
