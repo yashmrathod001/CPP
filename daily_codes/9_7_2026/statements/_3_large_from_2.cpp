2c28915a6a4925d95f60b2086e801178aa18d18e
yashmrathod001 <yashmrathod123@gmail.com>
2026-07-09 18:05:14 +0530

    large from 2

---
+++ daily_codes/9_7_2026/statements/_3_large_from_2.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int n1 , n2 ;
+	cout<< "enter two numbers :";
+	cin>>n1 >> n2;
+	if (n1>n2){
+		cout<<"\n"<<n1 <<"is large.";
+	} 
+	else
+	{
+		cout<<"\n"<<n2<<"is large."
+	}
+	return 0;
+}
