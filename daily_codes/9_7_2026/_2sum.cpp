2bfed922de2bd3504d0c647e1d73e308f35bb87e
yashmrathod001 <yashmrathod123@gmail.com>
2026-07-09 17:32:10 +0530

    variavle and arithmetic operetops practiove

---
+++ daily_codes/9_7_2026/_2sum.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){
+	int n1 , n2 , sum ;
+	cout<<"enter 2 numbers :\n";
+	cin>>n1 >>n2;
+	sum = n1+n2;
+	cout<<"the sum of given two numbers is " << sum;
+	
+	
+	return 0;
+}
---
+++ daily_codes/9_7_2026/_3arearac.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int l, b , area;
+	cout << "enter  the lenght of hight and width of the ractcangls ";
+	cin>> l >> b ;
+	
+	area = l*b;
+	cout << " the area of ractagne is " << area ;
+	return 0;
+	}    
