8f148cd1b73745740c284edabb67e2b6932a4813
yashmrathod001 <yashmrathod123@gmail.com>
2026-07-10 16:52:33 +0530

    print 0 to n

---
+++ daily_codes/10_7_2026/_1_print_1TO10.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+		for(int i=0;i <10;i++)
+	{
+		cout<< i+1<<"\n";
+	}
+	cout<<" now print 10 to 1 \n";
+	
+	for(int i=10;i > 0;i--)
+	{
+		cout<< i<<"\n";
+	}
+	
+
+
+	return 0;
+}
+
