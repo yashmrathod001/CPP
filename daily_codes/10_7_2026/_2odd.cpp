aac9f3506ece2b679d3086a3a660e44a5d2ead88
yashmrathod001 <yashmrathod123@gmail.com>
2026-07-10 16:53:46 +0530

    find odd e

---
+++ daily_codes/10_7_2026/_2odd.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int n = 100;
+	cout<<"the list of odd numbers up to 100 :\n";
+	for(int i=1;i <=100 ; i++)
+	{
+		
+		if(i%2==0)
+		{
+			cout<<i<<",";
+		}
+	}
+	cout<< "the  list of even numvers upto 100 :\n";	
+		for(int i=1;i <=100 ; i++)
+	{
+		
+		if(i%2==0)
+		{
+			cout<<i<<",";
+		}
+	}
+	
+
+	
+
+
+	return 0;
+}
