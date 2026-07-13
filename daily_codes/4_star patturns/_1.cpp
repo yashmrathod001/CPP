5d9fb9e44044b395af723a17c09e70fc50e923be
yashmrathod001 <yashmrathod123@gmail.com>
2026-07-13 19:31:37 +0530

    first patturn

---
+++ daily_codes/4_star patturns/_1.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main(){
+	int n=4;
+	cout<<"the first pattur\n";
+	
+	for(int i=1;i<=n;i++)
+	{
+		for(int i=1;i<=n;i++)	
+		{
+			cout<<i;
+		}
+		cout<<"\n";
+	}
+	
+	return 0;
+}
